839e8d653642929548cdcac3c6e410a3e8edabe4
Sam Gluck <sdgluck@gmail.com>
2016-08-28 10:39:06 +0100

    v0.0.1

---
+++ README.md
@@ -0,0 +1,100 @@
+# react-floating-suffix
+
+> :cloud: Don't let that suffix outta sight!
+
+Made with ❤ at [@outlandish](http://www.twitter.com/outlandish)
+
+<a href="http://badge.fury.io/js/react-floating-suffix"><img alt="npm version" src="https://badge.fury.io/js/react-floating-suffix.svg"></a>
+[![js-standard-style](https://img.shields.io/badge/code%20style-standard-brightgreen.svg)](http://standardjs.com/)
+
+Float an extension/suffix/whatevs so that it displays to the right of the content even if the content overflows. 
+
+> Dude no. 1: "Hey man, you should totally float the suffix so that it never goes out of sight." 
+
+> Dude no. 2: ["Outta sight, dude!"](https://www.urbandictionary.com/define.php?term=Out%20of%20sight&defid=7866179)
+
+<img alt="react-floating-suffix demo" src="https://github.com/sdgluck/react-floating-suffix/blob/master/demo.gif">
+
+## Install
+
+```sh
+npm install react-floating-suffix --save
+```
+
+## Import
+
+```js
+// ES2015
+import FloatingSuffix from 'react-floating-suffix'
+```
+
+```js
+// CommonJS
+var FloatingSuffix = require('react-floating-suffix')
+```
+
+## Usage
+
+```js
+<FloatingSuffix
+  content="New Text Document"
+  suffix=".txt"
+  
+  // all optional
+  style={{ backgroundColor: 'white', color: 'black' }}
+  width='100px'
+  textOverflow="ellipsis"
+/>
+```
+
+## Props
+
+### `content <String|Element>`
+
+Required.
+
+The content to append a floating suffix on to.
+
+### `suffix <String>`
+
+Required.
+
+The floating suffix text.
+
+### `style <Object>`
+
+Optional, default: none.
+
+Customise the container with your own CSS styles.
+
+Overrides the `textOverflow` and `width` properties.
+
+### `width <String|Number>`
+
+Optional, default: `'100%'`
+
+The width of the container. 
+
+When the content is larger than this value minus the width of the suffix, the suffix will stick.
+
+```js
+// Awesome pseudo-code
+const willSuffixFloat = container.width - suffix.width > width 
+```
+
+### `textOverflow <String>`
+
+Optional, default: `ellipsis`.
+
+Value of the `text-overflow` CSS property for the `content`.
+
+## Contributing
+
+All pull requests and issues welcome!
+
+If you're not sure how, check out Kent C. Dodds'
+[great video tutorials on egghead.io](https://egghead.io/lessons/javascript-identifying-how-to-contribute-to-an-open-source-project-on-github)!
+
+## Author & License
+
+`react-floating-suffix` was created by [Sam Gluck](https://twitter.com/sdgluck) and is released under the MIT license.
---
+++ index.js
@@ -0,0 +1,114 @@
+'use strict'
+
+var React = require('react')
+
+var PropTypes = React.PropTypes
+var el = React.createElement
+
+var requiredStringOrElement = PropTypes.oneOfType([
+  React.PropTypes.string.isRequired,
+  React.PropTypes.element.isRequired
+])
+
+var optionalStringOrNumber = PropTypes.oneOfType([
+  React.PropTypes.string,
+  React.PropTypes.number
+])
+
+var FloatingSuffix = React.createClass({
+  displayName: 'FloatingSuffix',
+
+  propTypes: {
+    content: requiredStringOrElement,
+    suffix: requiredStringOrElement,
+    width: optionalStringOrNumber,
+    textOverflow: PropTypes.string,
+    style: PropTypes.object
+  },
+
+  floatingSuffixStyle: {
+    position: 'absolute',
+    top: 0,
+    right: 0,
+    backgroundColor: 'inherit'
+  },
+
+  getDefaultProps: function () {
+    return {
+      width: '100%',
+      textOverflow: 'ellipsis',
+      style: {
+        display: 'inline-block',
+        float: 'left',
+        position: 'relative',
+        overflow: 'hidden',
+        backgroundColor: 'white'
+      }
+    }
+  },
+
+  shouldFloatSuffix: function () {
+    var refs = this.refs
+    if (refs.content && refs.suffix) {
+      var boundary = refs.container.offsetWidth - refs.suffix.offsetWidth - 10
+      return refs.content.offsetWidth >= boundary
+    }
+  },
+
+  componentDidMount: function () {
+    if (this.shouldFloatSuffix()) {
+      var refs = this.refs
+      var contentWidth = refs.container.offsetWidth - refs.suffix.offsetWidth
+      refs.content.style.width = contentWidth + 'px'
+      Object.assign(this.refs.suffix.style, this.floatingSuffixStyle)
+    }
+  },
+
+  render: function () {
+    const width = this.props.width
+    const textOverflow = this.props.textOverflow
+
+    const containerProps = {
+      ref: 'container',
+      style: Object.assign({},
+        { width: width },
+        FloatingSuffix.getDefaultProps().style,
+        this.props.style)
+    }
+
+    const contentProps = {
+      key: 'content',
+      ref: 'content',
+      style: {
+        display: 'inline-block',
+        float: 'left',
+        whiteSpace: 'nowrap',
+        textOverflow: textOverflow,
+        WebkitTextOverflow: textOverflow,
+        overflow: 'hidden'
+      }
+    }
+
+    const suffixProps = {
+      key: 'suffix',
+      ref: 'suffix',
+      style: {}
+    }
+
+    if (this.shouldFloatSuffix()) {
+      var refs = this.refs
+      var contentWidth = refs.container.offsetWidth - refs.suffix.offsetWidth
+      refs.content.style.width = contentWidth + 'px'
+      suffixProps.style = this.floatingSuffixStyle
+    }
+
+    return (
+      el('div', containerProps, [
+        el('span', contentProps, this.props.content),
+        el('span', suffixProps, this.props.suffix)
+      ])
+    )
+  }
+})
+
+module.exports = FloatingSuffix
